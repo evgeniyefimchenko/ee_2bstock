e71ba9913876b8046be6a9374103f767d4de94eb
Евгений Ефимченко <evgeniy@efimchenko.ru>
2021-04-27 21:42:53 +0300

    fix sql query

--- app/addons/ee_2bstock/Tygh/Addons/EE2bstock/EE2bstock.php
+++ app/addons/ee_2bstock/Tygh/Addons/EE2bstock/EE2bstock.php
@@ -5,88 +5,151 @@ namespace Tygh\Addons\EE2bstock;
 use Tygh\Registry;
 use Tygh\Api\Response;
 
+/**
+* Ответ функции имеет следующие обязательные поля в массиве.
+* status_code - код статуса ответа int
+* response - массив ответа array
+* error - статус ошибки boolean
+* error_text - текст ошибки если имеется str
+*/
+
 class EE2bstock {
     
 	public $module_oprions = [];
-	private $error;
+	public $error = false;
+	public $error_text = '';
+	public $status_code;
+	private $response = ['status_code' => 0, 'response' => [], 'error' => false, 'error_text' => ''];
+	private $data;
+	private $method;
+	private $cart_config;
+	private $mysqli;
 	
-	public function __construct () {
+	public function __construct($data, $method) {
+		$this->data = $data;
+		$this->method = $method;
+		$this->status_code = $this->get_status_code_by_method();
+		$this->template_response([]);
 		$this->module_oprions = Registry::get('addons.ee_2bstock');
-		$this->error = false;
+		$this->cart_config = Registry::get('config');
+		$this->mysqli = new \mysqli($this->cart_config['db_host'], $this->cart_config['db_user'], $this->cart_config['db_password'], $this->cart_config['db_name']);
     }
 
-	public function features($data, $method) {		
-		switch ($method) {
+	public function features() {		
+		$pref = $this->get_pref_func_by_method();
+		$func_name = $pref . '_features';
+		//die($func_name);
+		return $this->template_response($this->$func_name());
+	}
+	
+	// Функции характеристик
+	private function get_features() {		
+		$response = [];
+		try {
+			if ((isset($this->data['external_id']) && mb_strlen($this->data['external_id']) > 5) || (isset($this->data['id']) && mb_strlen($this->data['id']) > 0)) {			
+				$response = db_get_row('SELECT * FROM ?:product_features as f, ?:product_features_descriptions as fd, ?:product_features_values as fv
+				WHERE f.feature_id = ?i OR f.external_id LIKE ?s AND f.feature_id = fd.feature_id AND f.feature_id = fv.feature_id GROUP BY f.feature_id LIMIT 1', $v['id'], $this->data['external_id']);
+			} else {
+				$this->mysqli->real_query('SELECT * FROM ' . $this->cart_config['table_prefix'] . 'product_features as f, ' . $this->cart_config['table_prefix'] . 'product_features_descriptions as fd, ' . $this->cart_config['table_prefix'] . 'product_features_values as fv 
+				WHERE f.feature_id = fd.feature_id AND f.feature_id = fv.feature_id GROUP BY f.feature_id');		
+			}
+			
+			$res_mysqli = $this->mysqli->use_result();			
+			while ($row = $res_mysqli->fetch_assoc()) {
+				$response[] = $row;
+			}			
+			
+			if ($response && !empty($response['feature_id'])) { // Одна характеристика, допишем все её варианты
+				$response['variants'] = db_get_array('SELECT * FROM ?:product_feature_variants as v, ?:product_feature_variant_descriptions  as vd
+				WHERE v.feature_id =?i AND vd.variant_id = v.variant_id GROUP BY v.variant_id');
+			}
+		} catch (Exception $e) {
+			$this->error_text = $e->getMessage();
+			$this->error = true;
+			$this->status_code = Response::STATUS_INTERNAL_SERVER_ERROR;
+		}
+		return $response;
+	}
+	
+	private function update_features() {
+		$response = [];
+		try {
+			
+		} catch (Exception $e) {
+			$this->error_text = $e->getMessage();
+			$this->error = true;
+			$this->status_code = Response::STATUS_INTERNAL_SERVER_ERROR;
+		}		
+	}
+	
+	private function create_features() {
+		$response = [];
+		try {
+			
+		} catch (Exception $e) {
+			$this->error_text = $e->getMessage();
+			$this->error = true;
+			$this->status_code = Response::STATUS_INTERNAL_SERVER_ERROR;
+		}		
+	}
+	
+	private function dell_features() {
+		$response = [];
+		try {
+			
+		} catch (Exception $e) {
+			$this->error_text = $e->getMessage();
+			$this->error = true;
+			$this->status_code = Response::STATUS_INTERNAL_SERVER_ERROR;
+		}		
+	}
+	
+	// Функции вариантов характеристик
+	
+	private function get_pref_func_by_method() {
+		switch ($this->method) {
 			case 'GET':
-				$response = $this->get_features($data);
+				$response = 'get';
 				break;
 			case 'PUT':
-				$response = $this->update_features($data);
+				$response = 'update';
 				break;
 			case 'POST':
-				$response = $this->create_features($data);
+				$response = 'create';
 				break;
 			case 'DELETE':
-				$response = $this->dell_features($data);
+				$response = 'dell';
 				break;
 		}
-		$status_code = $this->get_status_code($method);
-		$resp = ['status_code' => $status_code, 'response' => $response];
-		return !$this->error ? $resp : $this->error;
+		return $response;		
 	}
 	
-	private function get_features($data) {
-		$res = false;
-		if ((isset($data['external_id']) && mb_strlen($data['external_id']) > 5) || (isset($data['id']) && mb_strlen($data['id']) > 0)) {
-			$res = db_get_row('SELECT * FROM ?:product_features as f, ?:product_features_descriptions as fd, ?:product_features_values as fv
-			WHERE f.feature_id = ?i OR f.external_id LIKE ?s AND f.feature_id = fd.feature_id AND f.feature_id = fv.feature_id GROUP BY f.feature_id LIMIT 1', $data['id'], $data['external_id']);
-		} else {
-			$res = db_get_array('SELECT * FROM ?:product_features as f, ?:product_features_descriptions as fd, ?:product_features_values as fv
-			WHERE f.feature_id = fd.feature_id AND f.feature_id = fv.feature_id GROUP BY f.feature_id');		
-		}
-		if ($res && !empty($res['feature_id'])) { // Одна характеристика, допишем все её варианты
-			$res['variants'] = db_get_array('SELECT ');
-		}
-		return $res;
-	}
-	
-	private function update_features($data) {
-		
-	}
-	
-	private function create_features($data) {
-		
-	}
-	
-	private function dell_features($data) {
-		
-	}
-	
-	/*
-	public function features_variants($data, $method) {
-		
-		$status_code = $this->get_status_code($method);
-		$resp = ['status_code' => $status_code, 'response' => $response];
-		return !$this->error ? $resp : $this->error;
-	}
-	*/
-	
-	private function get_status_code($method) {
-		if ($method == 'GET' || $method == 'PUT') {
+	private function get_status_code_by_method() {
+		if ($this->method == 'GET' || $this->method == 'PUT') {
 			$status_code = Response::STATUS_OK;
-		} elseif ($method == 'POST') {
+		} elseif ($this->method == 'POST') {
 			$status_code = Response::STATUS_CREATED;
-		} elseif ($method == 'DELETE') {
+		} elseif ($this->method == 'DELETE') {
 			$status_code = Response::STATUS_NO_CONTENT;
 		} else {
-			$this->error = ['status_code' => 400, 'error' => 400, 'text_error' => 'Запрос с недопустимым методом: ' . $method, 'comments' => ['REQUEST'=> $_REQUEST]];
+			$this->error = true;			
+			$this->error_text = 'Неверный метод запроса ' . $this->method;			
 			$status_code = Response::STATUS_BAD_REQUEST;
 		}
 		return $status_code;	
 	}
+	
+	private function template_response($func_result) {		
+		 if ($this->error) {
+			 $func_result = ['REQUEST' => var_export($this->data, true)];
+		 }
+		 $this->response = ['status_code' => $this->status_code, 'response' => $func_result, 'error' => $this->error, 'error_text' => $this->error_text];
+		 return $this->response;
+	}
 }
 
 /**
+	требуемые таблицы
 	cscart_product_features
 	cscart_product_features_descriptions
 	cscart_product_features_values
--- app/addons/ee_2bstock/controllers/frontend/ee_2bstock.php
+++ app/addons/ee_2bstock/controllers/frontend/ee_2bstock.php
@@ -3,71 +3,83 @@ use Tygh\Registry;
 use Tygh\Api\Response;
 use Tygh\Api\Request;
 use Tygh\Addons\EE2bstock\EE2bstock;
-
+/**
+* admin@makeshop.pro
+* Ml3179C572b3Jfo03R20GYr9z0l7I23X
+*/
 $module_oprions = Registry::get('addons.ee_2bstock');
 
-$auth['user'] = $_SERVER['PHP_AUTH_USER'];
-$auth['api_key'] = $_SERVER['PHP_AUTH_PW'];
+$request = new Request();	
+$content_type = $request->getContentType();
+$accept_type = $request->getAcceptType();
+$method = $request->getMethod();
+unset($request);
 
-$_REQUEST = array_merge(json_decode(file_get_contents('php://input'), true), $_REQUEST);
-$_REQUEST['CONTENT_TYPE'] = $_SERVER['CONTENT_TYPE'];
+$auth['user'] = $_SERVER['PHP_AUTH_USER'] ? $_SERVER['PHP_AUTH_USER'] : $_REQUEST['user'];
+$auth['api_key'] = $_SERVER['PHP_AUTH_PW'] ? $_SERVER['PHP_AUTH_PW'] : $_REQUEST['api_key'];
+
+$_REQUEST['CONTENT_TYPE'] = $content_type;
 $_REQUEST['HTTP_ACCEPT'] = $_SERVER['HTTP_ACCEPT'];
 $_REQUEST['PHP_AUTH_PW'] = $auth['api_key'];
 $_REQUEST['PHP_AUTH_USER'] = $auth['user'];
-$_REQUEST = filter_input_array(INPUT_REQUEST, array_map('trim', $_REQUEST));
+$_REQUEST['method'] = $method;
 
-$log_path = DIR_ROOT . '/app/addons/ee_2bstock/Tygh/Addons/EE2bstock/logs/last_request.txt';
-file_put_contents($log_path, var_export($_REQUEST, true), LOCK_EX);
+//die(var_export($_REQUEST, true));
+
+$log_path = DIR_ROOT . '/app/addons/ee_2bstock/Tygh/Addons/EE2bstock/logs/';
+file_put_contents($log_path . last_request.txt, var_export($_REQUEST, true), LOCK_EX);
 
 if ($module_oprions['ee_2bstock_https'] == 'Y' && !defined('HTTPS')) {
-	$resp = ['error' => 403, 'text_error' => 'Разрешен только HTTPS протокол магазина.', 'comments' => ['REQUEST'=> $_REQUEST]];	
-	$status_response = STATUS_FORBIDDEN;	
+	$resp = ['error' => true, 'status_code' => 403, 'text_error' => 'Разрешен только HTTPS протокол магазина.', 'response' => ['REQUEST'=> $_REQUEST]];	
+	$status_response = Response::STATUS_FORBIDDEN;	
 }
 
 if (!isset($auth['user']) && !isset($auth['user']) && !$error) {
-	$resp = ['error' => 401, 'text_error' => 'Нет данных для авторизации.', 'comments' => ['REQUEST'=> $_REQUEST]];	
+	$resp = ['error' => true, 'status_code' => 401, 'text_error' => 'Нет данных для авторизации.', 'response' => ['REQUEST'=> $_REQUEST]];	
 	$status_response = Response::STATUS_UNAUTHORIZED;
 } else {
 	$user_data = fn_get_api_user($auth['user'], $auth['api_key']);
 	if (isset($user_data['status']) && $user_data['status'] == 'A' || ($module_oprions['ee_2bstock_admin'] == 'Y' && $user_data['user_type'] == 'A')) {
-		$request = new Request();	
-		$content_type = $request->getContentType();
-		$accept_type = $request->getAcceptType();
-		$method = $request->getMethod();
-		unset($request);
-		if ($content_type != 'application/json') {
-			$resp = ['error' => 400, 'text_error' => 'Неверный формат запроса.', 'comments' => ['REQUEST'=> $_REQUEST]];
+		if ($content_type != 'application/json' && $module_oprions['ee_2bstock_full_log'] != 'Y') {
+			$resp = ['error' => true, 'status_code' => 400, 'text_error' => 'Неверный формат запроса.', 'response' => ['REQUEST'=> $_REQUEST]];
 			$status_response = Response::STATUS_BAD_REQUEST;	
 		} else {		
-			$EE2bstock = new EE2bstock();
-			if (isset($mode) && isset($action) && method_exists($EE2bstock, $mode . '_' . $action)) {
-				$func_name = $mode . '_' . $action;
-				$resp = $EE2bstock->$func_name($_REQUEST, $method);
-				$status_response = $resp['status_code'];
-			} elseif (isset($mode) && method_exists($EE2bstock, $mode)) {
-				$resp = $EE2bstock->$mode($_REQUEST, $method);
-				$status_response = $resp['status_code'];
+			$EE2bstock = new EE2bstock($_REQUEST, $method);
+			if (!$EE2bstock->error) {
+				if (isset($mode) && isset($action) && method_exists($EE2bstock, $mode . '_' . $action)) {
+					$func_name = $mode . '_' . $action;
+					//die($func_name);
+					$resp = $EE2bstock->$func_name();
+					$status_response = $resp['status_code'];
+				} elseif (isset($mode) && method_exists($EE2bstock, $mode)) {
+					//die($mode);
+					$resp = $EE2bstock->$mode();
+					$status_response = $resp['status_code'];
+				} else {
+					$resp = ['error' => true, 'status_code' => 403, 'text_error' => 'Необходимо указать сущность(пример: ' . fn_url('index.php') . '2bstock/features) и передать достаточные POST параметры.', 'response' => ['REQUEST'=> $_REQUEST]];	
+					$status_response = Response::STATUS_FORBIDDEN;				
+				}
 			} else {
-				$resp = ['error' => 403, 'text_error' => 'Необходимо указать сущность(пример: ' . fn_url('index.php') . '2bstock/features) и передать достаточные POST параметры.', 'comments' => ['REQUEST'=> $_REQUEST]];	
-				$status_response = Response::STATUS_FORBIDDEN;				
+				$resp = $EE2bstock->response;
+				$status_response = $resp['status_code'];
 			}
 		}		
 	} else {
-		$resp = ['error' => 401, 'text_error' => 'Нет допустимых прав для пользователя ' . $auth['user'], 'comments' => ['REQUEST'=> $_REQUEST]];	
+		$resp = ['error' => true, 'status_code' => 401, 'text_error' => 'Нет допустимых прав для пользователя ' . $auth['user'], 'response' => ['REQUEST'=> $_REQUEST]];	
 		$status_response = Response::STATUS_UNAUTHORIZED;	
 	}
 
 }
 
 if ($module_oprions['ee_2bstock_error_log'] == 'Y' && isset($resp['error'])) {
-	file_put_contents(__DIR__ . '/logs/errors.txt', PHP_EOL . date("Y-m-d H:i:s") . PHP_EOL . 'Ошибки:' . PHP_EOL . var_export($resp, true) . PHP_EOL, FILE_APPEND | LOCK_EX);
+	file_put_contents($log_path . 'errors.txt', PHP_EOL . date("Y-m-d H:i:s") . PHP_EOL . 'Ошибки:' . PHP_EOL . var_export($resp, true) . PHP_EOL, FILE_APPEND | LOCK_EX);
 }
 
 if ($module_oprions['ee_2bstock_full_log'] == 'Y') {
-	file_put_contents(__DIR__ . '/logs/errors.txt', PHP_EOL . date("Y-m-d H:i:s") . PHP_EOL . 'SERVER: ' . PHP_EOL .  var_export($_SERVER) . PHP_EOL . 'Ответ-Запрос:' . PHP_EOL . var_export($resp, true) . PHP_EOL, FILE_APPEND | LOCK_EX);	
+	echo json_encode($resp, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+	file_put_contents($log_path . '/full_logs.txt', PHP_EOL . date("Y-m-d H:i:s") . PHP_EOL . 'SERVER: ' . PHP_EOL .  var_export($_SERVER, true) . PHP_EOL . 'Ответ-Запрос:' . PHP_EOL . var_export($resp, true) . PHP_EOL, FILE_APPEND | LOCK_EX);	
 }
 
-echo json_encode($resp);
 $response = new Response($status_response);
-$response->send();		
-exit;
+$response->send();
+die;
